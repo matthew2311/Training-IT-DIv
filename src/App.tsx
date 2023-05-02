db4990019a856f4cf00b30468d0271e6dfc065e9
Matthew Sebastian <matthew.sebastian@binus.edu>
2023-05-02 14:50:46 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './Test/Test';
 
 function App() {
   return (
@@ -10,17 +11,13 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Test></Test>
+        {/* <Test /> */}
       </header>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/Test/Test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Test() {
+    return (
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Learn React
+        </a>
+    );
+    // alt + shift + f
+}
+
+export default Test;
+
+
